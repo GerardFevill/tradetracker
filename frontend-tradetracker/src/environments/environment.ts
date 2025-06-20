f824d2def3e5653c4c98e0848b635ce9e03f0850
GerardFevill <gerard.nouglozeh@protonmail.com>
2025-06-20 00:44:34 +0000

    env update

--- frontend-tradetracker/src/environments/environment.ts
+++ frontend-tradetracker/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // Environnement par défaut (développement)
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.56.10:3001/api', // URL directe vers l'API
+  apiUrl: 'http://192.168.56.10:3000/api', // URL directe vers l'API
   appName: 'TradeTracker (Dev)'
 };
